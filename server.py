f9cb3ef9229c2f17d6ef1629cef312c861b1ca86
Daniel T <stephane.tchangang@gmail.com>
2024-04-11 18:21:45 -0700

    lib: install pypdf

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask, request, redirect, url_for
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
-import pikepdf
 import PyPDF2
+
 load_dotenv()  # take environment variables from .env.
 OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 
@@ -14,36 +14,36 @@ client = OpenAI(
     api_key=OPEN_AI_API_KEY,
 )
 
-upload_page = '''
+upload_page = """
 <form action="/upload_pdf" method="post" enctype="multipart/form-data">
     <label for="file">Choose a file to upload:</label>
     <input type="file" id="file" name="file">
     <br>
     <input type="submit" value="Upload File">
 </form>
-'''
-
+"""
 
 
 @app.route("/upload_pdf", methods=["POST"])
 def upload():
     if request.method == "POST":
-        file = request.files.get('file')
+        file = request.files.get("file")
 
         reader = PyPDF2.PdfReader(file)
-    
+
         # Initialize an empty string to collect text
-        text = ''
-    
+        text = ""
+
         # Iterate over each page
         for page_num in range(len(reader.pages)):
             # Get a specific page in the PDF
             page = reader.pages[page_num]
-            
+
             # Extract text from the page
             text += page.extract_text()
         return summarize_text(text)
-    
+
+
 @app.route("/")
 def hello_world():
     return upload_page
@@ -63,6 +63,7 @@ def get_input():
 
         result = chat_completion.choices[0].message.content
         return {"result": result}
+
 
 def summarize_text(text: str):
     messageBase = {
